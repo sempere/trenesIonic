ba1c37caa5b54d2d9402a288e56e922c7af2cdf2
Álvaro Sempere <biyonder.os@gmail.com>
2020-01-30 12:31:14 +0100

    Pruebas con firebase antes de integracion con Google Calendar

--- src/app/services/trenes.service.ts
+++ src/app/services/trenes.service.ts
@@ -17,4 +17,20 @@ export class TrenesService {
     return this.fireStore.collection("trenes", ref => ref.where('$doc.id', '==','tren1'))
     .valueChanges();
   }
+
+  public createData() {
+    var data = {
+      prueba : "Hola",
+      campo1 : 3,
+      campo2 : [{
+        campo21: 3,
+        campo22: "Mi amigo Pablo es un cara"
+      },
+      {
+        campo21: 311,
+        campo22: "Mi 33333 Pablo 3f3f3 un cara"
+      }] 
+    };
+    return this.fireStore.collection('trenes').doc("tren1").set(data); 
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -12,10 +12,18 @@ export class Tab2Page {
   public todos;
 
   constructor(private trenesService : TrenesService) {
+    this.todos = {};
+    this.todos.campo2 = { campo21 : 3343 };
     console.debug;
     trenesService.getTrenes().subscribe(res => {
       this.todos = res;
       console.debug;
+    });
+
+    trenesService.createData().then(() => {
+      console.debug;
+    }, (error) => {
+      console.error(error);
     });
   }
 
